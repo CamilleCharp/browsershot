a3dba13178057d874f377b3786008a3273f5e66b
Camille Charpentier <camille.charpentier77@gmail.com>
2022-10-20 16:35:34 +0200

    Added background and size configuration.

--- view/bundle.js
+++ view/bundle.js
@@ -7902,10 +7902,33 @@ var __webpack_exports__ = {};
 /* harmony import */ var html2canvas__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(html2canvas__WEBPACK_IMPORTED_MODULE_0__);
 
 
+const BODY_BG_COLOR = "#171717";
+const DEFAULT_SCREEN_BG_COLOR = "#BBC9DC";
+
 //Elements selection
 const screenArea = document.querySelector(".screen-area");
-const backgroundPicker = document.querySelector('input[type="color"');
+const screenSize = {width: Math.floor(screenArea.getBoundingClientRect().width), height: Math.floor(screenArea.getBoundingClientRect().height)};
+const backgroundPicker = document.querySelector('input[name="background-picker"');
+const widthPicker = document.querySelector('input[name="screen-width"]');
+const heightPicker = document.querySelector('input[name="screen-height"]');
 const dlButton = document.querySelector(".dl-button");
+
+//Elements state
+document.body.style.backgroundColor = BODY_BG_COLOR;
+screenArea.style.backgroundColor = DEFAULT_SCREEN_BG_COLOR
+
+
+//Inputs state
+widthPicker.value = screenSize.width;
+heightPicker.value = screenSize.height;
+backgroundPicker.value = DEFAULT_SCREEN_BG_COLOR;
+
+//config helper
+const IS_LANDSCAPE = screenSize.width > screenSize.height;
+const SCREEN_RATIO = IS_LANDSCAPE ? screenSize.width / screenSize.height : screenSize.height / screenSize.width
+
+const getHeight = (width) => Math.floor(IS_LANDSCAPE ? width / SCREEN_RATIO : width * SCREEN_RATIO);
+const getWidth = (height) => Math.floor(IS_LANDSCAPE ? height * SCREEN_RATIO : height / SCREEN_RATIO);
 
 //Get the data for the preview and add it to the page.
 chrome.runtime.sendMessage('get_screen_info', res => {
@@ -7918,15 +7941,24 @@ chrome.runtime.sendMessage('get_screen_info', res => {
         screenshot.src = res.url
     }
 })
-console.log(backgroundPicker)
-//Change the background with the background picker input
+//Config listeners
 backgroundPicker.addEventListener('input', (e) => {
     screenArea.style.backgroundColor = e.target.value
 })
 
+widthPicker.addEventListener('change', (e) => {
+    screenArea.style.width = e.target.value + "px";
+    screenArea.style.height = getHeight(e.target.value) + "px";
+    heightPicker.value = Math.ceil(screenArea.getBoundingClientRect().height)
+})
+
+heightPicker.addEventListener('change', (e) => {
+    screenArea.style.height = e.target.value + "px";
+    screenArea.style.width = getWidth(e.target.value) + "px";
+    widthPicker.value = Math.ceil(screenArea.getBoundingClientRect().width)
+})
+
 //Select the screen area and the download button, on click convert to canvas, create a link with the base64Url and save it. 
-
-
 dlButton.addEventListener('click', async (e) => {
     html2canvas__WEBPACK_IMPORTED_MODULE_0___default()(screenArea).then((canvas) => {
         const dlLink = document.createElement('a');
